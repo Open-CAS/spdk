f358143c2fe4a9330fdc68cae707c32c4f050f25
klapinsk <katarzyna.lapinska@intel.com>
2021-06-07 14:16:00 +0200

    Add recovery functional tests
    Signed-off-by: klapinsk <katarzyna.lapinska@intel.com>

--- test/ocf/ocf.sh
+++ test/ocf/ocf.sh
@@ -5,6 +5,9 @@ rootdir=$(readlink -f $testdir/../..)
 
 source $rootdir/test/common/autotest_common.sh
 
+run_test "ocf_recovery_from_ram_wrong_disk" "$testdir/recovery/recovery-from-ram-wrong-disk.sh"
+run_test "ocf_recovery_from_ram_neg" "$testdir/recovery/recovery-from-ram-neg.sh"
+run_test "ocf_recovery_from_disk" "$testdir/recovery/recovery-from-disk.sh"
 run_test "ocf_fio_modes" "$testdir/integrity/fio-modes.sh"
 run_test "ocf_bdevperf_iotypes" "$testdir/integrity/bdevperf-iotypes.sh"
 run_test "ocf_stats" "$testdir/integrity/stats.sh"
